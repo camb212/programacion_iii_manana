72fc080b20f4cca4125b74947687fbef726a3529
IDIC <alumnos@alumnosuio.ute.edu.ec>
2025-10-16 09:57:38 -0500

    Completadas las tareas de la semana 3

---
+++ modulo2/03_operadores.js
@@ -0,0 +1,37 @@
+console.log("OPERADORES ARITMETICOS:");
+
+numero1 = 20;
+numero2 = 5;
+console.log("suma");
+suma = numero1 + numero2;
+console.log("Resultado de la suma:", suma);
+console.log("resta");
+resta = numero1 - numero2;
+console.log("Resultado de la resta:", resta);
+console.log("multiplicacion");
+multiplicacion = numero1 * numero2;
+console.log("Resultado de la multiplicacion:", multiplicacion);
+console.log("division");
+division = numero1 / numero2;
+console.log("Resultado de la division:", division);
+modulo = numero1%numero2;
+console.log("Resultado de la division:", division);
+
+console.log("Resultado:", modulo);
+potencia = numero1*numero2;
+console.log("Resultado", potencia);
+
+console.log("operadores comparacion");
+console.log("igualdad debil ==","5"==5);
+console.log("igualdad estricta ==","5"==5);
+console.log("igualdad debil !==","5"==5);
+console.log("igualdad estricta  !==","5"==5);
+console.log("mayor que >","5">5);
+console.log("menor que <","5"<5);
+console.log("mayor que o igual >","5">5);
+console.log("menor que o igual <","5"<5);
+
+console.log("operadores logicos");
+console.log("y And &&","5"<=5 && "5">=5);
+console.log("o or ||","5"<=5||"6">=4);
+console.log("negacion !", !(5=5));
